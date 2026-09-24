c0c9bcfd6aeec0f3f635f66f33b8b2cdd4c3fbf5
ezeAst <166661587+ezeAst@users.noreply.github.com>
2026-09-24 16:24:45 -0500

    Update backend configuration in backend.tf

--- infra/backend.tf
+++ infra/backend.tf
@@ -3,5 +3,8 @@ terraform {
   backend "s3" {
     # Bloqueo por archivo en el propio bucket. Requiere Terraform 1.10 o superior.
     use_lockfile = true
+    bucket = "inf384-tfstate-20213298"
+    key    = "lab3/terraform.tfstate"
+    region = "us-east-1"
   }
 }
