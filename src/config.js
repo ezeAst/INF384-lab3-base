f07d8071a66b9d88038800c45d78e94fc3a8bda7
ezeAst <166661587+ezeAst@users.noreply.github.com>
2026-09-24 17:44:29 -0500

    Delete src/config.js

--- src/config.js
+++
@@ -1,5 +0,0 @@
-// INYECCION 1: credencial escrita a proposito para probar el gate de secretos
-const AWS_ACCESS_KEY_ID = "AKIA4ZQEATQGBGE5GYAC";
-const AWS_SECRET_ACCESS_KEY = "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";
-
-module.exports = { AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY };
